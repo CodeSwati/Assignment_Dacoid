0a8acc01ffca4ffcad1a8809b50f03c9159dd942
CodeSwati <swatij280301@gmail.com>
2024-04-14 17:59:46 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,11 @@ function App() {
   return (
 
     <div className='w-[375px] h-[44px] sm:w-full sm:h-full  font-sans p-4'>
-      <Status/>
+    
 
       <Router>
         <Routes >
-        
+        <Route path='/' element={<Status/>}/>
           <Route path='/page1' element={<Page1/>}/>
           <Route path='/page2' element={<Page2/>}/>
           <Route path='/createPage' element={<CreateAccount/>}/>
