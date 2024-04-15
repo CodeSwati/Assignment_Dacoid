43e4073cdf90639f0e5314f14f4a6f543373b400
CodeSwati <swatij280301@gmail.com>
2024-04-15 14:10:34 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,23 @@ import Welcome from './components/Welcome'
 import GoalPage from './components/GoalPage'
 import Tracker from './components/Tracker'
 import Schedule from './components/Schedule'
-
+import { PiCellSignalFullBold } from "react-icons/pi";
+import { IoIosWifi } from "react-icons/io";
+import { IoBatteryFullOutline } from "react-icons/io5";
 function App() {
   
 
   return (
 
-    <div className='w-[375px] h-[44px] sm:w-full sm:h-full  font-sans p-4'>
-    
+    <div className='w-[375px] h-[44px] sm:w-full sm:h-full flex  flex-col font-sans p-4'>
+       <div className=' flex justify-between'>
+          <p className='font-semibold text-sm'>9:41</p>
+          <div className='flex gap-2'>
+            <PiCellSignalFullBold />
+            <IoIosWifi />
+            <IoBatteryFullOutline />
+          </div>
+      </div>
 
       <Router>
         <Routes >
--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import { PiCellSignalFullBold } from "react-icons/pi";
-import { IoIosWifi } from "react-icons/io";
-import { IoBatteryFullOutline } from "react-icons/io5";
+
 import { Link } from 'react-router-dom';
 
 
@@ -9,16 +7,9 @@ function Status() {
 
   
   return (
-   <div className='flex w-full flex-col gap-20'>
-      <div className=' flex justify-between'>
-          <p className='font-semibold text-sm'>9:41</p>
-          <div className='flex gap-1'>
-            <PiCellSignalFullBold />
-            <IoIosWifi />
-            <IoBatteryFullOutline />
-          </div>
-      </div>
-      <Link to="/page1" className=' text-center text-[#9FB2FF] underline 
+   <div className='w-full flex justify-center mt-40'>
+      
+      <Link to="/page1" className=' text-[#9FB2FF] underline 
       underline-offset-2'>Skip this dummy page</Link>
 
     </div>
